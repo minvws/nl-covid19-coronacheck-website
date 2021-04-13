3596299fc32a39950677bf57edb99d892b7f0665
Jeroen Breen <jeroen@innouveau.nl>
2021-04-13 09:12:23 +0200

    add gitignore

---
+++ print-portal/src/components/mixins/CurrentLanguage.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'CurrentLanguage',
+    components: {},
+    computed: {},
+    methods: {}
+}
+</script>
+
+<template>
+    <div class="CurrentLanguage">
+        CurrentLanguage
+    </div>
+</template>
+
+<style lang="scss">
+    .CurrentLanguage {
+
+    }
+</style>
