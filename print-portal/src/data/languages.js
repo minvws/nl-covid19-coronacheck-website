f3c4271059a71ab68db103833dacbe84cab9031c
Jeroen Breen <jeroen@innouveau.nl>
2021-04-14 15:09:23 +0200

    remove languages

--- print-portal/src/data/languages.js
+++ print-portal/src/data/languages.js
@@ -4,16 +4,6 @@ const languages = [
         name: 'Nederlands',
         locale: 'nl',
         direction: 'ltr'
-    }, {
-        id: 2,
-        name: 'English',
-        locale: 'en',
-        direction: 'ltr'
-    }, {
-        id: 3,
-        name: 'عربى',
-        locale: 'ar',
-        direction: 'rtl'
     }
 ];
 
