0748cef61d5692b13ba201cf35577e5bed9250cf
Jeroen Breen <jeroen@innouveau.nl>
2021-05-03 12:24:23 +0200

    scroll to top when changing route

--- print-portal/src/router/index.js
+++ print-portal/src/router/index.js
@@ -36,7 +36,11 @@ const routes = [
 const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
-    routes
+    routes,
+    scrollBehavior (to, from, savedPosition) {
+        console.log(savedPosition);
+        return { x: 0, y: 0 };
+    }
 })
 
 router.beforeEach((to, from, next) => {
