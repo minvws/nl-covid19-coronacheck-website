8bbdb324b6b9a12928549d04a20053d175ccfcf6
Jeroen Breen <jeroen@innouveau.nl>
2021-05-03 11:29:34 +0200

    add skiplinks

---
+++ print-portal/src/components/elements/Skiplinks.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'Skiplinks',
+    components: {},
+    computed: {},
+    methods: {}
+}
+</script>
+
+<template>
+    <div class="skiplinks">
+        <a href="#content">
+            Ga direct naar inhoud
+        </a>
+    </div>
+</template>
+
+<style lang="scss">
+.Skiplinks {
+
+}
+</style>
