c64e9a47bd71752b006274d5726f37cc0773f26b
Jeroen Breen <jeroen@innouveau.nl>
2021-05-06 16:22:32 +0200

    use translated badges

--- print-portal/src/components/elements/PreferMobile.vue
+++ print-portal/src/components/elements/PreferMobile.vue
@@ -27,7 +27,7 @@ export default {
                         class="app-store"
                         href="https://apps.apple.com/nl/app/coronacheck/id1548269870"
                         target="_blank">
-                        <img src="assets/img/app-store-badges/app-store-badge-nl.svg"/>
+                        <img :src="'assets/img/app-store-badges/app-store-badge-' + currentLanguage.locale + '.svg'"/>
                     </a>
                 </li>
                 <li class="app_badge_column__right">
@@ -35,7 +35,7 @@ export default {
                         class="play-store"
                         href="https://play.google.com/store/apps/details?id=nl.rijksoverheid.ctr.holder"
                         target="_blank">
-                        <img src="assets/img/app-store-badges/google-play-badge-nl.svg"/>
+                        <img :src="'assets/img/app-store-badges/google-play-badge-' + currentLanguage.locale + '.svg'"/>
                     </a>
                 </li>
             </ul>
