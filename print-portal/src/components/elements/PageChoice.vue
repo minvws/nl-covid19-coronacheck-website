665ee6b9519faee08f50924f151af519fa220518
Jeroen Breen <jeroen@innouveau.nl>
2021-05-20 10:51:07 +0200

    add arrow

--- print-portal/src/components/elements/PageChoice.vue
+++ print-portal/src/components/elements/PageChoice.vue
@@ -30,31 +30,47 @@ export default {
         <div class="PageChoice__body">
             {{body}}
         </div>
+        <div class="PageChoice__icon">
+            <img src="assets/img/icons/next.svg"/>
+        </div>
     </div>
 </template>
 
 <style lang="scss">
-    @import "@/styles/variables/index";
+@import "@/styles/variables/index";
 
-    .PageChoice {
-        background: $color-lightgrey;
-        margin-bottom: 24px;
-        padding: $grid-x2;
-        border-radius: 9px;
-        box-shadow: 0 2px 6px rgba(0,0,0,0.15);
-        cursor: pointer;
+.PageChoice {
+    background: $color-lightgrey;
+    margin-bottom: 24px;
+    padding: $grid-x2;
+    border-radius: 9px;
+    box-shadow: 0 2px 6px rgba(0,0,0,0.15);
+    cursor: pointer;
+    position: relative;
 
-        &:hover {
-            background: $color-mediumgrey;
-        }
+    &:hover {
+        background: $color-mediumgrey;
+    }
 
-        &:last-child {
-            margin-bottom: 0;
-        }
+    &:last-child {
+        margin-bottom: 0;
+    }
 
-        &__header {
-            font-weight: 700;
-            margin-bottom: $grid-x1;
+    &__header {
+        font-weight: 700;
+        margin-bottom: $grid-x1;
+    }
+
+    &__icon {
+        position: absolute;
+        right: 25px;
+        top: 50%;
+        transform: translateY(-50%);
+
+        img {
+            width: 12px;
+            display: block;
         }
     }
+}
 </style>
