27706f475be675900246fd8d244642205800937f
Jeroen Breen <jeroen@innouveau.nl>
2021-06-23 22:54:48 +0200

    fix decoding

--- print-portal/src/tools/cms.js
+++ print-portal/src/tools/cms.js
@@ -1,3 +1,10 @@
+function b64DecodeUnicode(str) {
+    // Going backwards: from bytestream, to percent-encoding, to original string.
+    return decodeURIComponent(atob(str).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+}
+
 export const cmsDecode = (encoded) => {
-    return JSON.parse(atob(encoded));
+    return JSON.parse(b64DecodeUnicode(encoded));
 }
