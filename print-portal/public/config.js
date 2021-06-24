07d75e8f564977bebf5dd3a3e0eb9f204dc52d4f
Jeroen Breen <jeroen@innouveau.nl>
2021-06-24 17:31:57 +0200

    too busy via config

--- print-portal/public/config.js
+++ print-portal/public/config.js
@@ -1,5 +1,6 @@
 window.config = {
     api: 'https://holder-api.acc.coronacheck.nl/v4',
     tvs: 'https://tvs.acc.coronacheck.nl',
-    accessTokens: 'https://holder-api.acc.coronacheck.nl/v4/holder/access_tokens'
+    accessTokens: 'https://holder-api.acc.coronacheck.nl/v4/holder/access_tokens',
+    tooBusy: false
 }
