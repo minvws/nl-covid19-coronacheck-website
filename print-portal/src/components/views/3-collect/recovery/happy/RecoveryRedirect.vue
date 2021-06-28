53a9b5a9ca35ae9742791d1f8912e60bd5db7bc2
Jeroen Breen <jeroen@innouveau.nl>
2021-06-28 13:37:42 +0200

    collect only positive tests for now

--- print-portal/src/components/views/3-collect/recovery/happy/RecoveryRedirect.vue
+++ print-portal/src/components/views/3-collect/recovery/happy/RecoveryRedirect.vue
@@ -11,7 +11,7 @@ export default {
     data() {
         return {
             isLoading: false,
-            filter: 'recovery,positivetest',
+            filter: 'positivetest',
             pages: {
                 cancel: 'ChoiceProof',
                 previous: 'CollectRecovery',
