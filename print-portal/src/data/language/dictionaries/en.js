a3bfb7cc21a306723c49a47f20f8f506cd3fb99c
Jeroen Breen <jeroen@innouveau.nl>
2021-06-30 20:54:18 +0200

    fix key

--- print-portal/src/data/language/dictionaries/en.js
+++ print-portal/src/data/language/dictionaries/en.js
@@ -106,7 +106,7 @@ const en = {
         'print': {
             'openPDF': 'Open PDF'
         },
-        'PrintNegativeTest': {
+        'printNegativeTest': {
             'both': {
                 'pageHeader': 'Print your certificates',
                 'pageIntro': '<p><b>Your certificates</b><br>You’ve made a test certificate, in the form of  a QR code. You’ve got a code for the Netherlands and one for other countries.</p><p><b>Information in the QR</b><br><a href="https://coronacheck.nl/en/faq/1-6-welke-informatie-staat-in-mijn-qr-code/" target="_blank">Read here</a> which information the QR code contains</p><p><b>What now?</b><br>Print the PDF and bring it with you to the location or activity you’re visiting. Or to the country you’re travelling to.</p>'
