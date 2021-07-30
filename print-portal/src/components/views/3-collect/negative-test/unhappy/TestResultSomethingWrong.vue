4892c1fae4b93356070bfe51a932f8a4b48ab452
Jeroen Breen <jeroen@innouveau.nl>
2021-07-30 15:18:19 +0200

    Add something wrong page

---
+++ print-portal/src/components/views/3-collect/negative-test/unhappy/TestResultSomethingWrong.vue
@@ -0,0 +1,33 @@
+<script>
+import Page from '@/components/elements/Page';
+import PageIntro from '@/components/elements/PageIntro';
+import CcButton from '@/components/elements/CcButton';
+
+export default {
+    name: 'TestResultSomethingWrong',
+    components: { Page, PageIntro, CcButton },
+    methods: {
+        goHome() {
+            this.$router.push({ name: 'Home' })
+        }
+    }
+}
+</script>
+
+<template>
+    <Page
+        :display-back-button="false">
+        <div class="section">
+            <PageIntro
+                :head="$t('views.testResultSomethingWrong.pageHeader')"
+                :intro="$t('views.testResultSomethingWrong.pageIntro')"/>
+            <div class="section-block">
+                <div class="section-block__footer">
+                    <CcButton
+                        @select="goHome()"
+                        :label="$t('goBackToStart')"/>
+                </div>
+            </div>
+        </div>
+    </Page>
+</template>
