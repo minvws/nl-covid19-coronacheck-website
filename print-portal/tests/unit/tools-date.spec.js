dd227e9a43e4b1606eb126cc7bf0db2c2abb14f8
Jeroen Breen <jeroen@innouveau.nl>
2021-08-03 11:23:37 +0200

    Update unit tests for date

--- print-portal/tests/unit/tools-date.spec.js
+++ print-portal/tests/unit/tools-date.spec.js
@@ -1,22 +1,61 @@
-import { shallowMount } from '@vue/test-utils'
 import dateTool from '@/tools/date.js'
 
 describe('Date tool', () => {
-    it('Standard formatting of a date', () => {
-        const dateString = '2020-04-01 12:05';
-        const dateStringExpected = '01-04-2020 12:05';
-        expect(dateTool.dateToString(dateString)).toMatch(dateStringExpected)
+    it('Date with day NL', () => {
+        const dateString = '2020-03-01 12:05';
+        const dateStringExpected = 'zondag 1 maart 2020';
+        expect(dateTool.dateToString(dateString, 'date-with-day', 'nl')).toMatch(dateStringExpected)
     })
 
-    it('Formatting of a date used for a testresult without language setting', () => {
-        const dateString = '2021-04-13 14:00';
-        const dateStringExpected = 'dinsdag 13 april 14:00';
-        expect(dateTool.dateToString(dateString, 'datetime-with-day')).toMatch(dateStringExpected)
+    it('Date with day EN', () => {
+        const dateString = '2020-03-01 12:05';
+        const dateStringExpected = 'Sunday 1 March 2020';
+        expect(dateTool.dateToString(dateString, 'date-with-day', 'en')).toMatch(dateStringExpected)
     })
 
-    it('Formatting of a date used for a testresult, english', () => {
-        const dateString = '2021-04-13 14:00';
-        const dateStringExpected = 'Tuesday 13 April 14:00';
+    it('Date without day NL', () => {
+        const dateString = '2020-03-01 12:05';
+        const dateStringExpected = '1 maart 2020';
+        expect(dateTool.dateToString(dateString, 'date-without-day', 'nl')).toMatch(dateStringExpected)
+    })
+
+    it('Date without day EN', () => {
+        const dateString = '2020-03-01 12:05';
+        const dateStringExpected = '1 March 2020';
+        expect(dateTool.dateToString(dateString, 'date-without-day', 'en')).toMatch(dateStringExpected)
+    })
+
+    it('Datetime with day NL', () => {
+        const dateString = '2020-03-01 12:05';
+        const dateStringExpected = 'zondag 1 maart 12:05';
+        expect(dateTool.dateToString(dateString, 'datetime-with-day', 'nl')).toMatch(dateStringExpected)
+    })
+
+    it('Datetime with day EN', () => {
+        const dateString = '2020-03-01 12:05';
+        const dateStringExpected = 'Sunday 1 March 12:05';
         expect(dateTool.dateToString(dateString, 'datetime-with-day', 'en')).toMatch(dateStringExpected)
+    })
+
+    it('Date to monthname NL', () => {
+        const dateString = '2020-03-01 12:05';
+        const dateStringExpected = 'maart';
+        expect(dateTool.dateToMonthName(dateString, 'nl')).toMatch(dateStringExpected)
+    })
+
+    it('Date to monthname EN', () => {
+        const dateString = '2020-03-01 12:05';
+        const dateStringExpected = 'March';
+        expect(dateTool.dateToMonthName(dateString, 'en')).toMatch(dateStringExpected)
+    })
+
+    it('Valid string should pass', () => {
+        const dateString = '2020-03-01';
+        expect(dateTool.isValidDateString(dateString)).toBeTruthy()
+    })
+
+    it('Invalid string should fail', () => {
+        const dateString = '2020-xx-01';
+        expect(dateTool.isValidDateString(dateString)).toBeFalsy()
     })
 })
