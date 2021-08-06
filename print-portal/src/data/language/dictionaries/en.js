afb4f4b92ae1c1c1961c15014cd1975c5832d446
Jeroen Breen <jeroen@innouveau.nl>
2021-08-06 17:06:09 +0200

    Add EN copy for errorNoEvents

--- print-portal/src/data/language/dictionaries/en.js
+++ print-portal/src/data/language/dictionaries/en.js
@@ -145,6 +145,10 @@ const en = {
             'pageHeader': 'It\'s very busy at the moment',
             'pageIntro': '<p>Try again later today.</p>'
         },
+        'errorNoEvents': {
+            'pageHeader': 'Sorry, your details were not retrieved',
+            'pageIntro': '<p>Due to increased traffic or an outage at one of the involved parties, some details of your %{type} may be missing. </p><p>Tip: wait one hour and try again.</p>'
+        },
         'errorTokenFlow': {
             'pageHeader': 'Sorry, something is going wrong',
             'pageIntro': '<p>Something is going wrong at the test location. Therefore, your details can’t be retrieved.</p><h4>What now?</h4><p>Contact the test location and give them the following error code:</p><p><strong>Error code: %{error}</strong></p>'
