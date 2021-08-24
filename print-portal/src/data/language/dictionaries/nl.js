f3c25e52bdef0d5fae2473c57bebe7944eac4560
Jeroen Breen <jeroen@innouveau.nl>
2021-08-24 11:08:23 +0200

    remove unused keys

--- print-portal/src/data/language/dictionaries/nl.js
+++ print-portal/src/data/language/dictionaries/nl.js
@@ -149,10 +149,6 @@ const nl = {
         //     'pageHeader': 'Sorry, er gaat iets mis',
         //     'pageIntro': '<p>Er gaat iets mis bij het ophalen van je gegevens. Neem contact op met de helpdesk van CoronaCheck via 0800 -1421 (gratis). En geef de volgende foutcode door:</p><p><strong>Foutcode: %{error}</strong></p>'
         // },
-        // 'errorAccessTokens': {
-        //     'pageHeader': 'Sorry, er gaat iets mis',
-        //     'pageIntro': '<p>Neem contact op met de helpdesk van CoronaCheck via 0800 -1421 (gratis). En geef de volgende foutcode door</p><p><strong>Foutcode: %{error}</strong></p>'
-        // },
         'errorNoEvents': {
             'pageHeader': 'Sorry, het is niet gelukt',
             'pageIntro': '<p>Door drukte of een storing bij een van de partijen hebben we geen gegevens over je %{type} kunnen ophalen.</p><p>Tip: wacht een uur en probeer het dan nog een keer.</p>'
@@ -336,10 +332,6 @@ const nl = {
             'collectEventsWithErrors': {
                 'head': 'Zijn dit al je vaccinaties?',
                 'body': '<p>Door een storing of drukte zijn mogelijk niet al jouw gegevens opgehaald. Mist er een vaccinatie? Kom later terug en haal opnieuw je gegevens op.</p>'
-            },
-            'errorAccessTokens': {
-                'head': 'Sorry er gaat iets mis',
-                'body': 'Probeer het later nog eens. Of bekijk actuele storingen op <a href="https://status.coronacheck.nl/">status.coronacheck.nl.</a><br>(Foutcode: %{code})'
             }
         }
     },
