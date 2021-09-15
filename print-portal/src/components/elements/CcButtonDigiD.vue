2a93d16204421845df3d18e721a03c284755af5d
Jeroen Breen <jeroen@innouveau.nl>
2021-09-15 17:41:20 +0200

    Add component for digid button

---
+++ print-portal/src/components/elements/CcButtonDigiD.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    name: 'CcButtonDigiD',
+    methods: {
+        onClick() {
+            this.$emit('select')
+        }
+    }
+}
+</script>
+
+<template>
+    <button
+        type="button"
+        @click="onClick"
+        class="btn button-digid">
+        <div class="btn__content">
+            {{$t('components.digid.loginDigid')}}
+            <img alt="logo DigiD" src="assets/img/digid/logo_digid_rgb.svg"/>
+        </div>
+    </button>
+</template>
+
+<style lang="scss">
+.button-digid {
+    padding: 13px 32px 13px 48px;
+
+    .btn__content {
+        display: flex;
+        align-items: center;
+
+        img {
+            width: 20px;
+            height: auto;
+            margin-left: 12px;
+        }
+    }
+}
+</style>
