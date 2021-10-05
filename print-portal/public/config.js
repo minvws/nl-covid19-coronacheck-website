e2296164b1ef897e7febb06ce25939a65c8a830b
Jeroen Breen <jeroen@innouveau.nl>
2021-10-05 08:37:25 +0200

    switch to v6

--- print-portal/public/config.js
+++ print-portal/public/config.js
@@ -1,5 +1,5 @@
 window.config = {
-    api: 'https://holder-api.acc.coronacheck.nl/v4',
+    api: 'https://holder-api.acc.coronacheck.nl/v6',
     tvs: 'https://tvs.acc.coronacheck.nl',
     accessTokens: 'https://holder-api.acc.coronacheck.nl/v4/holder/access_tokens',
     tooBusy: false
