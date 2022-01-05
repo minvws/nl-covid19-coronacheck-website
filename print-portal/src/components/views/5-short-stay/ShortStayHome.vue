b4bc82c1c70269dcce4584424e221e2593d32d86
5upr3m0 <remo@egeniq.com>
2022-01-05 10:54:23 +0100

    feat(shot-stay): add component

---
+++ print-portal/src/components/views/5-short-stay/ShortStayHome.vue
@@ -0,0 +1,22 @@
+<script>
+import Page from '@/components/elements/Page';
+import PageIntro from '@/components/elements/PageIntro';
+
+export default {
+    name: 'ShortStarHome',
+    components: { Page, PageIntro }
+}
+</script>
+
+<template>
+    <Page :display-back-button="false">
+        <div class="section">
+            <PageIntro
+                :head="$t('views.shortStay.pageHeader')"
+                :intro="$t('views.shortStay.pageIntro')"
+            />
+            <div class="section-block">
+            </div>
+        </div>
+    </Page>
+</template>
--- print-portal/src/router/pages/other.js
+++ print-portal/src/router/pages/other.js
@@ -1,5 +1,6 @@
 import Home from '@/components/views/1-home/Home.vue'
 import ChoiceProof from '@/components/views/2-a-choice-proof/ChoiceProof'
+import ShortStayHome from '@/components/views/5-short-stay/ShortStayHome'
 
 const otherPages = [{
     path: '/',
@@ -9,6 +10,12 @@ const otherPages = [{
     path: '/keuze-papieren-bewijs',
     component: ChoiceProof,
     name: 'ChoiceProof'
-}]
+},
+{
+    path: '/kort-verblijf',
+    component: ShortStayHome,
+    name: 'ShortStay'
+}
+]
 
 export default otherPages
