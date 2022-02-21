d8b303d9a298e0270f742bc295a798eb5c76a92b
5upr3m0 <remo@egeniq.com>
2022-02-21 16:46:10 +0100

    TAIGA-104: feat(ie11): show NotSupportedBrowser page when on ie11

---
+++ print-portal/src/components/views/error-pages/NotSupportedBrowser.vue
@@ -0,0 +1,40 @@
+<script>
+import Page from '@/components/elements/Page';
+import PageIntro from '@/components/elements/PageIntro';
+import { homeLink } from '@/tools/router';
+
+export default {
+    name: 'BrowserNotSupported',
+    components: {
+        Page,
+        PageIntro
+    },
+    computed: {
+        home () {
+            return homeLink()
+        }
+    }
+}
+</script>
+
+<template>
+    <Page
+        class="home">
+        <div class="section">
+            <PageIntro
+                :head="$t('views.browserNotSupported.pageHeader')"
+                :intro="$t('views.browserNotSupported.pageIntro')"/>
+            <div class="section-block">
+                <a class="link" :href="home">{{ $t('views.browserNotSupported.button') }}</a>
+            </div>
+        </div>
+    </Page>
+</template>
+<style scoped lang="scss">
+.link {
+    display: inline-block;
+    padding-top: 8px;
+    font-weight: bold;
+    text-decoration: none;
+}
+</style>
--- print-portal/src/router/pages/other.js
+++ print-portal/src/router/pages/other.js
@@ -1,14 +1,25 @@
-import Home from '@/components/views/1-home/Home.vue'
-import ChoiceProof from '@/components/views/2-a-choice-proof/ChoiceProof'
+import Home from '@/components/views/1-home/Home.vue';
+import ChoiceProof from '@/components/views/2-a-choice-proof/ChoiceProof';
+import NotSupportedBrowser from '@/components/views/error-pages/NotSupportedBrowser.vue'
 
-const otherPages = [{
-    path: '/',
-    component: Home,
-    name: 'Home'
-}, {
-    path: '/keuze-papieren-bewijs',
-    component: ChoiceProof,
-    name: 'ChoiceProof'
-}]
+export const BROWSER_NOT_SUPPORTED = 'BrowserNotSupported'
 
-export default otherPages
+const otherPages = [
+    {
+        path: '/',
+        component: Home,
+        name: 'Home'
+    },
+    {
+        path: '/keuze-papieren-bewijs',
+        component: ChoiceProof,
+        name: 'ChoiceProof'
+    },
+    {
+        path: '/te-oude-internetbrowser',
+        component: NotSupportedBrowser,
+        name: BROWSER_NOT_SUPPORTED
+    }
+];
+
+export default otherPages;
