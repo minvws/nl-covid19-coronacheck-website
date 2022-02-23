d4d7dc5cec9b36064e370603e794b0acfcf3b281
5upr3m0 <remo@egeniq.com>
2022-02-23 13:35:46 +0100

    TAIGA-3756: fix(PrintFaq): change order for 1G

--- print-portal/src/components/views/4-print/PrintFaq.vue
+++ print-portal/src/components/views/4-print/PrintFaq.vue
@@ -35,10 +35,12 @@ export default {
             const details = []
 
             if (this.type === FilterTypes.VACCINATION) {
-                details.push('whyNoDutchCertificate');
+                if (!this.$store.getters.is1G) details.push('whyNoDutchCertificate');
                 if (this.regionType === RegionTypes.EUROPEAN) {
-                    if (this.$store.getters.is1G) details.push('whyNoDomesticVaccination1G');
-                    else details.push('whyNoDomesticVaccination');
+                    if (this.$store.getters.is1G) {
+                        details.push('whyNoDomesticVaccination1G');
+                        details.push('whyNoDutchCertificate');
+                    } else details.push('whyNoDomesticVaccination');
                 }
             }
             if (this.type === FilterTypes.RECOVERY && this.regionType === RegionTypes.EUROPEAN) {
