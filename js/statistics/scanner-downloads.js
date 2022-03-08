0e69a6738d52c5d649dbe55970ad3a90a1a701d0
Anneke Sinnema <mail@annekesinnema.nl>
2022-03-08 10:35:45 +0100

    Updated statistics for week 9
    Signed-off-by: Anneke Sinnema <mail@annekesinnema.nl>

--- js/statistics/scanner-downloads.js
+++ js/statistics/scanner-downloads.js
@@ -48,5 +48,6 @@ CCScanner_downloads_plot = [
     {"downloadnr": 2.937211, "date": "2022-02-06"},
     {"downloadnr": 3.002054, "date": "2022-02-13"},
     {"downloadnr": 3.034991, "date": "2022-02-20"},
-    {"downloadnr": 3.055455, "date": "2022-02-27"},
+    {"downloadnr": 3.055458, "date": "2022-02-27"},
+    {"downloadnr": 3.067385, "date": "2022-03-06"}
 ]
