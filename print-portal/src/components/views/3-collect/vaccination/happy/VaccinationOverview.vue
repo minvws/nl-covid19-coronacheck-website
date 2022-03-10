f6a4db0edd62dd5ec23c00122ff9f410d6c74098
5upr3m0 <remo@egeniq.com>
2022-03-10 15:26:41 +0100

    3811:(vaccination-overview-mixin): refactor to mixin

--- print-portal/src/components/views/3-collect/vaccination/happy/VaccinationOverview.vue
+++ print-portal/src/components/views/3-collect/vaccination/happy/VaccinationOverview.vue
@@ -5,78 +5,20 @@ import Vaccination from './Vaccination';
 import CcButton from '@/components/elements/CcButton';
 import CcModestButton from '@/components/elements/CcModestButton';
 import overviewMixin from '@/components/views/3-collect/_shared/overview-mixin'
-import dateTool from '@/tools/date';
+import VaccinationOverviewMixin from '@/components/views/3-collect/_shared/vaccination-overview-mixin'
 import LoadingCover from '@/components/elements/LoadingCover';
 import { FilterTypes } from '@/types/filter-types'
-import { ProviderTypes } from '@/types/provider-types'
 
 export default {
     name: 'VaccinationOverview',
     components: { LoadingCover, Page, PageIntro, Vaccination, CcButton, CcModestButton },
-    mixins: [overviewMixin],
+    mixins: [overviewMixin, VaccinationOverviewMixin],
     data() {
         return {
             filter: FilterTypes.VACCINATION,
             pages: {
                 print: 'PrintVaccination'
             }
-        }
-    },
-    computed: {
-        signedEventSets() {
-            // for vaccination we put double data in one set
-            const signedEvents = this.$store.getters['signedEvents/getProofEvents'](this.filter);
-            const uniqueSignedEventSets = [];
-
-            const isTheSameEvent = (a, b) => {
-                const vaccinationA = a.event.vaccination;
-                const vaccinationB = b.event.vaccination;
-
-                const isOnSameDay = (a, b) => {
-                    const dayInMs = 1000 * 3600 * 24;
-                    return Math.abs(new Date(a.date).getTime() - new Date(b.date).getTime()) < dayInMs;
-                }
-
-                const isSameHpkCode = (a, b) => {
-                    return a.hpkCode.length > 0 && a.hpkCode === b.hpkCode;
-                }
-
-                const isSameManufacturer = (a, b) => {
-                    return a.manufacturer.length > 0 && a.manufacturer === b.manufacturer;
-                }
-
-                return isOnSameDay(vaccinationA, vaccinationB) &&
-                    (isSameHpkCode(vaccinationA, vaccinationB) || isSameManufacturer(vaccinationA, vaccinationB))
-            }
-
-            const getMatch = (signedEvent) => {
-                for (const set of uniqueSignedEventSets) {
-                    for (const item of set) {
-                        if (isTheSameEvent(signedEvent, item)) {
-                            return set;
-                        }
-                    }
-                }
-                return null;
-            }
-
-            for (const signedEvent of signedEvents) {
-                const match = getMatch(signedEvent);
-                if (!match) {
-                    // create an empty set
-                    uniqueSignedEventSets.push([signedEvent])
-                } else {
-                    // we want GGD to be the first
-                    if (signedEvent.providerIdentifier === ProviderTypes.GGD) {
-                        match.unshift(signedEvent)
-                    } else {
-                        match.push(signedEvent)
-                    }
-                }
-            }
-            return uniqueSignedEventSets.sort((a, b) => {
-                return dateTool.getTime(a[0].event[a[0].event.type].date) - dateTool.getTime(b[0].event[b[0].event.type].date);
-            })
         }
     }
 }
