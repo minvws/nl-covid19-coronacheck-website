fabeb8fe06a57ecca4964a2876ac026c07346522
5upr3m0 <remo@egeniq.com>
2022-03-28 09:10:24 +0200

    3026: feat(qr-scanner): add pages

--- print-portal/src/qr/pages/QRLetterCombinationPage.vue
+++ print-portal/src/qr/pages/QRLetterCombinationPage.vue
@@ -3,21 +3,49 @@
 <Page
     @back="$router.go(-1)">
     <div class="section">
+        <PageIntro v-bind="intro" />
         <div class="section-block">
-            Letter combination TODO
+            <PrintFaqLink v-if="link" v-bind="{ label, ...link }"/>
         </div>
     </div>
 </Page>
 
 </template>
 
-<script lang="ts">
+<script>
 import Vue from 'vue'
 import Page from '@/components/elements/Page.vue';
+import PageIntro from '@/components/elements/PageIntro.vue';
+import PrintFaqLink from '@/components/views/4-print/PrintFaqLink.vue'
+import { getPageHeaderIntro, getPageLink } from '@/qr/utils/QRPage'
 
 export default Vue.extend({
     components: {
-        Page
+        Page,
+        PageIntro,
+        PrintFaqLink
+    },
+    props: {
+        routes: {
+            type: Array,
+            required: false
+        },
+        link: {
+            type: Object,
+            required: false
+        }
+    },
+    computed: {
+        name () {
+            const { name } = this.$route
+            return name ?? ''
+        },
+        label () {
+            return getPageLink(this.name)
+        },
+        intro () {
+            return getPageHeaderIntro(this.name)
+        }
     }
 })
 </script>
