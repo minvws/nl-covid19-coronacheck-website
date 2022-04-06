4cf5d7381886ced4d96f53d302b1fcd889440f07
Anneke Sinnema <mail@annekesinnema.nl>
2022-04-06 10:03:31 +0200

    Update statistics
    Signed-off-by: Anneke Sinnema <mail@annekesinnema.nl>

--- js/statistics/coronacheck-downloads.js
+++ js/statistics/coronacheck-downloads.js
@@ -53,4 +53,5 @@ CC_downloads_plot = [
     {"downloadnr": 15.736349, "date": "2022-03-11"},
     {"downloadnr": 15.789335, "date": "2022-03-18"},
     {"downloadnr": 15.847837, "date": "2022-03-25"},
+    { "downloadnr": 15.887689, "date": "2022-03-30" }
 ]
