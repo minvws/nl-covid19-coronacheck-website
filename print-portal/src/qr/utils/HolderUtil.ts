b6600179499f9fdcdcf25ca459da0d8cb8792e1b
5upr3m0 <remo@egeniq.com>
2022-06-28 13:35:03 +0200

    TAIGA-4508: feat(holder): add HolderUtil

---
+++ print-portal/src/qr/utils/HolderUtil.ts
@@ -0,0 +1,44 @@
+import { decodeQRtoDCC } from '@/qr/utils/DCCDecoder'
+
+export type Holder = {
+    firstName: string;
+    lastName: string;
+    birthDate: string;
+}
+
+export type Event = {
+    holder: Holder;
+    event?: unknown;
+}
+
+const getFirstCharToUpperCase = (input = '') => {
+    const char = input?.[0]
+    return char?.toUpperCase()
+}
+
+const isFirstCharEqual = (a: string, b: string) => {
+    return getFirstCharToUpperCase(a) === getFirstCharToUpperCase(b)
+}
+
+const isNameMatch = (holder: Holder, target: Holder) => {
+    return isFirstCharEqual(holder.firstName, target.firstName) ||
+        isFirstCharEqual(holder.lastName, target.lastName)
+}
+
+const getHolderFromEvents = (events: Event[]) => {
+    return events.find(({ event }) => !!event)?.holder
+}
+
+const isDayAndMonthOfBirthEqual = (holder: Holder, target: Holder) => {
+    const a = new Date(holder.birthDate)
+    const b = new Date(target.birthDate)
+    return (a.getDate() === b.getDate()) && (a.getMonth() === b.getMonth())
+}
+
+export const isHolderEqual = (qr: string, events: Event[]) => {
+    const holder = getHolderFromEvents(events)
+    if (!holder) return false
+    const { result: { holder: target } } = decodeQRtoDCC(qr)
+    return isNameMatch(holder, target) &&
+        isDayAndMonthOfBirthEqual(holder, target)
+}
