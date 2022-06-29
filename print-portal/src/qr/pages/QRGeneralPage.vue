7f893cc641b8e2340189b17e88e31386e9439e85
5upr3m0 <remo@egeniq.com>
2022-06-29 08:51:33 +0200

    TAIGA-4508: feat(QRGeneralPage): add v-if CcButton

--- print-portal/src/qr/pages/QRGeneralPage.vue
+++ print-portal/src/qr/pages/QRGeneralPage.vue
@@ -7,6 +7,7 @@
         <div class="section-block">
             <QRScannerNav v-if="routes" :routes="routes" />
             <CcButton
+                v-if="next"
                 @select="$router.push(next)"
                 :label="button"
             />
