46a539bf01f8d13e80df9b32fc2c582f1dc4578a
5upr3m0 <remo@egeniq.com>
2022-08-04 09:56:35 +0200

    TAIGA-4693: fix(XSS): change ErrorDigiD and ServerBusy

--- print-portal/src/components/views/error-pages/ErrorDigiD.vue
+++ print-portal/src/components/views/error-pages/ErrorDigiD.vue
@@ -2,14 +2,21 @@
 import Page from '@/components/elements/Page';
 import PageIntro from '@/components/elements/PageIntro';
 import CcButton from '@/components/elements/CcButton';
+import ErrorList from '@/components/elements/ErrorList.vue';
 import { goHome } from '@/tools/router';
 
 export default {
     name: 'ErrorDigiD',
-    components: { Page, PageIntro, CcButton },
+    components: { Page, PageIntro, CcButton, ErrorList },
     methods: {
         goHome() {
             goHome();
+        }
+    },
+    computed: {
+        errors() {
+            const errors = this.$route.query.error ? [this.$route.query.error] : []
+            return errors.length ? errors : undefined
         }
     }
 }
@@ -21,7 +28,9 @@ export default {
         <div class="section">
             <PageIntro
                 :head="$t('views.errorDigiD.pageHeader')"
-                :intro="$t('views.errorDigiD.pageIntro', { error: $route.query.error })"/>
+                :intro="$t('views.errorDigiD.pageIntro')">
+                 <ErrorList v-if="errors" :errors="errors" />
+            </PageIntro>
             <div class="section-block">
                 <div class="section-block__footer">
                     <CcButton
--- print-portal/src/components/views/error-pages/ServerBusy.vue
+++ print-portal/src/components/views/error-pages/ServerBusy.vue
@@ -2,11 +2,22 @@
 import Page from '@/components/elements/Page';
 import PageIntro from '@/components/elements/PageIntro';
 import CcButton from '@/components/elements/CcButton';
+import ErrorList from '@/components/elements/ErrorList.vue';
 import { goHome } from '@/tools/router';
 
 export default {
     name: 'ServerBusy',
-    components: { Page, PageIntro, CcButton },
+    components: { Page, PageIntro, CcButton, ErrorList },
+    computed: {
+        errors() {
+            const errors = this.$route.query.error ? [this.$route.query.error] : []
+            return errors.length ? errors : undefined
+        },
+        link () {
+            const link = this.$t('views.serverBusy.link')
+            return link
+        }
+    },
     methods: {
         goHome() {
             goHome();
@@ -21,7 +32,14 @@ export default {
         <div class="section">
             <PageIntro
                 :head="$t('views.serverBusy.pageHeader')"
-                :intro="$t('views.serverBusy.pageIntro', { error: $route.query.error })"/>
+                :intro="$t('views.serverBusy.pageIntro')">
+                 <ErrorList v-if="errors" :errors="errors" />
+                 <template v-if="link">
+                    <br v-if="errors" />
+                    <div v-html="link" />
+                    <br>
+                </template>
+            </PageIntro>
             <div class="section-block">
                 <div class="section-block__footer">
                     <CcButton
