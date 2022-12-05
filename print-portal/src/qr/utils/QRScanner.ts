f85fed37d7843f9a03e7ead1f1c8aabad51eb00f
5upr3m0 <remo@egeniq.com>
2022-12-05 12:00:01 +0100

    TAIGA-5095: fix(date): print date from dcc

--- print-portal/src/qr/utils/QRScanner.ts
+++ print-portal/src/qr/utils/QRScanner.ts
@@ -63,6 +63,7 @@ export const isValidQR = (qr: string, events: Event[]) => {
     if (!isOfType(qr, FilterTypes.VACCINATION)) {
         return Promise.reject(ERROR_QR_INVALID_TYPE)
     }
+    console.log({ events, la: decodeQRtoDCC(qr) })
     return validateQR(qr, events);
 }
 
